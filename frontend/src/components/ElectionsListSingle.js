155e6f64d912cf0e9575d306d01f845fd16f63cc
ajibadeabd <43596899+ajibadeabd@users.noreply.github.com>
2022-12-03 14:27:01 +0100

    css adjustment

--- frontend/src/components/ElectionsListSingle.js
+++ frontend/src/components/ElectionsListSingle.js
@@ -8,11 +8,24 @@ const ElectionsListSingle = ({props}) => {
 
     let chartSeries = [21, 22, 10, 28, 16, 21, 13, 30, 22, 10, 28, 16, 21, 22, 10, 28, 16, 21, 13, 30, 22, 10, 28]
 
-    let [series, setSeries] = useState(chartSeries);
+    let [series, setSeries] = useState(chartSeries);         
 
     return (
-        <div className="h-[50px] w-full rounded-l-full rounded-r-full bg-violet-600 flex my-[2px] py-1" key={"div" + idx}>
-            <div className="w-1/2 py-3 pl-5 pr-2 h-full" key={"div-left" + idx}>
+        <div className="h-[70px]  rounded-l-full rounded-r-full bg-violet-600 flex my-[2px] py-1" key={"div" + idx} 
+        
+        
+        style={{
+            marginTop:"20px",
+            marginLeft:'3%',
+            width:'95%'
+        }}
+        >
+            <div className="w-1/2 py-3 pl-5 pr-0 h-full" key={"div-left" + idx}
+              
+        style={{
+        width:'55%'
+        }}
+            >
                 <div className="flex items-baseline justify-evenly gap-[1px] h-full" key={"bars" + idx}>
                     {
                         series.map((el,ind,arr) => {
@@ -25,7 +38,8 @@ const ElectionsListSingle = ({props}) => {
                 </div>
             </div>
             <div className="py-2 w-1/2" key={"div-right" + idx}>
-                <div className="border-l h-full border-gray-300 flex items-center justify-center" key={"container" + idx}>
+                <div className="border-l h-full border-gray-300 flex items-center justify-center" key={"container" + idx} 
+         >
                     <p className="font-medium text-sm text-gray-300" key={"p" + item}>
                         {item}
                     </p>
