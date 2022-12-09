e23a6d3b3d365186808a19af60f68851def7807f
ajibadeabd <43596899+ajibadeabd@users.noreply.github.com>
2022-12-09 23:22:13 +0100

    code update

--- frontend/src/components/CandidatesList.jsx
+++ frontend/src/components/CandidatesList.jsx
@@ -2,18 +2,28 @@ import './catList.css'
 import './cadlist.css'
 import Pack from './Pack';
 import Card from './Card';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {AiOutlinePlus,AiOutlineMinus} from 'react-icons/ai'
+import { useContractContext } from '../context/contractContext/contractContext';
+import { ethers } from "ethers";
 
 const CandidatesList = () => {
     const [showCandidate, setShowCandidate] = useState(false);
-    const catList = Array(4).fill('candidates')
+    const [candidate, setCandidate] = useState([]);
+    // const catList = Array(4).fill('candidates')
+    console.log(ethers.utils.formatEther(candidate[0]?.category))
+    console.log (candidate[0]?.category.hex
+        )
     const categoryName = Array(4).fill('categoryName')
+    const { GetCandidates } = useContractContext();
+    useEffect(()=>{
+        GetCandidates().then((res)=>setCandidate(res))
+             },[])
     return (
       <div className='categoryList__container w-full h-full flex flex-col justify-between py-4 px-4'>
           <h4 className='text-center text-violet-300 font-medium text-lg'>Candidates List</h4>
           {
-              catList.map((cad,ind)=>(
+              candidate.map((cad,ind)=>(
                   <Pack key={ind} className="cat__list"> 
                     <Card className='cad' onClick={()=>setShowCandidate(prev=>!prev)}>
                         <div>
@@ -25,12 +35,16 @@ const CandidatesList = () => {
                         </div>
                         {
                             showCandidate &&  <p className={`${showCandidate?"cad__answer":"cad__hide"}`}>
-                                {cad}
+                                {cad[1]}
+                                {/* {cad} */}
+                                
                         </p>
                         }
                         {
                             !showCandidate &&  <p className={`${showCandidate?"cad__answer":"cad__hide"}`}>
-                          {cad}
+                          {/* {cad} */}
+                          {cad[1]}
+
                         </p>
                         }
        
--- frontend/src/components/CategoryList.jsx
+++ frontend/src/components/CategoryList.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import "../output.css";
 import "./catList.css";
 import Pack from './Pack';
+import { useContractContext } from '../context/contractContext/contractContext';
+
 const CategoryList = () => {
-    const catList = Array(4).fill('category')
+  const { GetListOfCategory } = useContractContext();
+
+
+  const [catlist,setCatList]= useState([])
+    useEffect(()=>{
+      GetListOfCategory().then((res)=>{
+        console.log(res)
+        setCatList(res)
+      })
+    },[])
   return (
     <div className='categoryList w-full h-full flex flex-col justify-between py-4 px-4'>
         <h4 className='text-center text-violet-300 font-medium text-lg'>Category List</h4>
         {
-            catList.map(category=>(
-                <Pack className="cat__list">{category}</Pack>
-            ))
+            catlist.map(category=>
+              { 
+
+               console.log(category)
+                return  <Pack className="cat__list">{category}</Pack>
+           } )
         }
     </div>
   )
