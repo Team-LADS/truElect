cefdc92e5b4f74ccf30380fd60d00bad224f6401
SanCrystal <anyanwu.amanzearthur@gmail.com>
2022-12-10 00:43:11 +0100

    lil changes

--- frontend/src/components/CategoryList.jsx
+++ frontend/src/components/CategoryList.jsx
@@ -5,9 +5,6 @@ import Pack from './Pack';
 import { useContractContext } from '../context/contractContext/contractContext';
 
 const CategoryList = () => {
-<<<<<<< HEAD
-    const catList = Array(10).fill('category')
-=======
   const { GetListOfCategory } = useContractContext();
 
 
@@ -18,18 +15,17 @@ const CategoryList = () => {
         setCatList(res)
       })
     },[])
->>>>>>> 422cef29e705b03514dd385d0ea24204dc68fcb0
   return (
     <div className='categoryList w-full h-full flex flex-col justify-between py-4 px-4'>
         <h4 className='text-center text-violet-300 font-medium text-lg'>Category List</h4>
-        {
+        {/* {
             catlist.map(category=>
               { 
 
                console.log(category)
                 return  <Pack className="cat__list">{category}</Pack>
            } )
-        }
+        } */}
     </div>
   )
 }
