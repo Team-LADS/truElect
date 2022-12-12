e197b93f70ecb1ebd836870bb9fd419b12a46bbb
SanCrystal <anyanwu.amanzearthur@gmail.com>
2022-12-12 02:30:55 +0100

    cadidates done

--- frontend/src/components/CandidatesList.jsx
+++ frontend/src/components/CandidatesList.jsx
@@ -20,35 +20,40 @@ const CandidatesList = () => {
         GetListOfCategory().then((res)=>{
             setCategory(res)})
              },[])
+    
+    let filter = [];
+    let cat = [];
+    for (const cad of candidate){
+        if(cat.indexOf(category[cad[2]-1])== -1){
+            cat.push(category[cad[2]-1]);
+            filter.push([category[cad[2]-1],[cad]])
+        }else{
+            filter[cat.indexOf(category[cad[2]-1])][1].push(cad)
+        }
+    }
+
     return (
       <div className='categoryList__container w-full h-full flex flex-col justify-between py-4 px-4'>
           <h4 className='text-center text-violet-300 font-medium text-lg'>Candidates List</h4>
           {
-              candidate.map((cad,ind)=>(
+              filter.map((cad,ind)=>(
                   <Pack key={ind} className="cat__list"> 
                     <Card className='cad' onClick={()=>setShowCandidate(prev=>!prev)}>
                         <div>
-                            <h5 className='cad__question'>{category[cad[2]-1]}</h5>
+                            <h5 className='cad__question'>{cad[0]}</h5>
                             <button className="cad__icon" >
                                 {showCandidate? <AiOutlineMinus/>: <AiOutlinePlus/>
                                 }
                             </button>
                         </div>
                         {
-                            showCandidate &&  <p className={`${showCandidate?"cad__answer":"cad__hide"}`}>
-                                {cad[1]}
-                                {/* {cad} */}
-                                
-                        </p>
+                            showCandidate && <div className={`${showCandidate?"cad__answer":"cad__hide"}`}>
+                                { 
+                               cad[1].map((el,index)=><p data-id={el[0]} key={index}> {el[1]} </p>)
+                                }
+                            </div>
+                            
                         }
-                        {
-                            !showCandidate &&  <p className={`${showCandidate?"cad__answer":"cad__hide"}`}>
-                          {/* {cad} */}
-                          {cad[1]}
-
-                        </p>
-                        }
-       
 
                     </Card>
                   </Pack>
